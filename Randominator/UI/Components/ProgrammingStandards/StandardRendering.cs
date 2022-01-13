d65ae13c6778f60cdab9d69d5f2911bf1c9e6987
TehGM <TehGM@users.noreply.github.com>
2022-01-13 22:48:15 +0100

    Added initial page for the programming standard generator

---
+++ Randominator/UI/Components/ProgrammingStandards/StandardRendering.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Components;
+using TehGM.Randominator.Generators.ProgrammingStandards;
+
+namespace TehGM.Randominator.UI.Components.ProgrammingStandards
+{
+    public static class StandardRendering
+    {
+        public static MarkupString RenderCodeHtml(this IBracketsStyleFormatter formatter, string beforeBrackets, string withinBrackets, BracketsStyle style)
+        {
+            string result = formatter.Apply(beforeBrackets, withinBrackets, style);
+            return (MarkupString)result.Replace(Environment.NewLine, "<br/>").Replace(" ", "&nbsp;");
+        }
+
+        public static MarkupString RenderBracketsStyleText(this BracketsStyle style)
+        {
+            string result;
+            if (style == BracketsStyle.NoBrackets)
+                result = "you should never use any brackets, and use identation instead.";
+            else if (style == BracketsStyle.Clean)
+                result = "you should put both <code>{</code> and <code>}</code> brackets on separate lines.";
+            else if (style == BracketsStyle.JavaScript)
+                result = "you should put opening brackets <code>{</code> on the same line, but closing brackets <code>}</code> on a separate line.";
+            else
+                result = "you should put both <code>{</code> and <code>}</code> brackets on the same lines.";
+            return (MarkupString)result;
+        }
+
+        public static MarkupString ListNormalPrefixSuffixRules(this NamingStyle style, INamingStyleFormatter formatter)
+        {
+            List<string> rules = new List<string>(2);
+            if (!string.IsNullOrWhiteSpace(style.NormalPrefix))
+                rules.Add($"prefixed with <code>{formatter.ApplyToWord(style, style.NormalPrefix, true)}</code>");
+            if (!string.IsNullOrWhiteSpace(style.NormalSuffix))
+                rules.Add($"suffixed with <code>{formatter.ApplyToWord(style, style.NormalSuffix, false)}</code>");
+            return (MarkupString)string.Join(" and ", rules);
+        }
+
+        public static MarkupString ListHungarianPrefixSuffixRules(this NamingStyle style)
+        {
+            List<string> rules = new List<string>(2);
+            if (style.HungarianPrefix != HungarianPartStyle.None)
+                rules.Add($"{style.HungarianPrefix} word for prefix");
+            if (style.HungarianSuffix != HungarianPartStyle.None)
+                rules.Add($"{style.HungarianSuffix} word for suffix");
+            return (MarkupString)string.Join(" and also ", rules);
+
+            string GetHungarianStyleDescription(HungarianPartStyle style)
+            {
+                if (style == HungarianPartStyle.Short)
+                    return "short";
+                if (style == HungarianPartStyle.Long)
+                    return "long";
+                throw new NotSupportedException($"{nameof(GetHungarianStyleDescription)} doesn't support hungarian style {style}");
+            }
+        }
+    }
+}
