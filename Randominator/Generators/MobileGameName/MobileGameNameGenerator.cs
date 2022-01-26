f68cde79d4c60d76e3321ad0feb2166fa33f0362
TehGM <TehGM@users.noreply.github.com>
2022-01-26 19:10:43 +0100

    Prefix support + word count for game name generator

--- Randominator/Generators/MobileGameName/MobileGameNameGenerator.cs
+++ Randominator/Generators/MobileGameName/MobileGameNameGenerator.cs
@@ -18,43 +18,45 @@
             MobileGameNameOptions options = this._options.CurrentValue;
             this._log.LogDebug("Generating random game name. Total wrapper words in sets: {WrapperWordCount}", options.SharedWordSet.Count() + options.LeadingWordSet.Count() + options.TrailingWordSet.Count());
 
-            // grab leading segment
-            string leadingSegment = this.GetRandomValue(options.LeadingWordSet.Union(options.SharedWordSet));
-            leadingSegment = this.CapitalizeWrapperSegment(leadingSegment);
-            this._log.LogTrace("Leading segment generated: {SegmentValue}", leadingSegment);
+            ICollection<string> segments = new List<string>(5);
+            if (this._random.RollChance(options.PrefixChance))
+                this.BuildSegment("Leading Prefix", options.PrefixWordSet, true, ref segments);
+            this.BuildSegment("Leading", options.LeadingWordSet.Union(options.SharedWordSet), true, ref segments);
+            this.BuildSegment("Middle", options.MiddleWordSet, false, ref segments);
+            if (this._random.RollChance(options.PrefixChance))
+                this.BuildSegment("Trailing Prefix", options.PrefixWordSet, true, ref segments);
+            this.BuildSegment("Trailing", options.TrailingWordSet.Union(options.SharedWordSet), true, ref segments);
 
-            // grab middle segment
-            string middleSegment = this.GetRandomValue(options.MiddleWordSet).ToLowerInvariant();
-            this._log.LogTrace("Middle segment generated: {SegmentValue}", middleSegment);
-
-            // grab leading segment
-            string trailingSegment = this.GetRandomValue(options.TrailingWordSet.Union(options.SharedWordSet));
-            trailingSegment = this.CapitalizeWrapperSegment(trailingSegment);
-            this._log.LogTrace("Trailing segment generated: {SegmentValue}", trailingSegment);
-
-            // combine result
-            return string.Join(' ', leadingSegment, middleSegment, trailingSegment);
+            return string.Join(' ', segments);
         }
 
-        private string CapitalizeWrapperSegment(string value)
+        void BuildSegment(string name, IEnumerable<string> values, bool capitalize, ref ICollection<string> outputs)
+        {
+            string segment = this._random.GetRandomValue(values);
+            if (capitalize)
+                segment = this.CapitalizeSegment(segment);
+
+            if (!string.IsNullOrWhiteSpace(segment))
+            {
+                outputs.Add(segment);
+                this._log.LogTrace("{SegmentName} segment generated: {SegmentValue}", name, segment);
+            }
+            else
+                this._log.LogTrace("{SegmentName} segment empty", name);
+        }
+
+        private string CapitalizeSegment(string value)
         {
             string[] segmentParts = value.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
             for (int i = 0; i < segmentParts.Length; i++)
             {
+                if (string.IsNullOrWhiteSpace(segmentParts[i]))
+                    continue;
                 char[] wordChars = segmentParts[i].ToLowerInvariant().ToCharArray();
-                wordChars[0] = Char.ToUpperInvariant(wordChars[0]);
+                wordChars[0] = char.ToUpperInvariant(wordChars[0]);
                 segmentParts[i] = new string(wordChars);
             }
             return string.Join(' ', segmentParts);
-        }
-
-        private T GetRandomValue<T>(IEnumerable<T> collection)
-        {
-            if (collection?.Any() != true)
-                return default;
-
-            int index = this._random.GetRandomNumber(0, collection.Count());
-            return collection.ElementAtOrDefault(index);
         }
     }
 }
