9100ab571e5f3d4a20a1e1f52f9d5ab92127e941
TehGM <TehGM@users.noreply.github.com>
2022-02-04 19:49:10 +0100

    Added support for google structured data

---
+++ Randominator/Generators/GeneratorType.cs
@@ -0,0 +1,12 @@
+﻿using TehGM.Randominator.UI;
+
+namespace TehGM.Randominator.Generators
+{
+    public enum GeneratorType
+    {
+        [StructuredDataValue("EntertainmentApplication")]
+        Meme,
+        [StructuredDataValue("UtilitiesApplication")]
+        Utility
+    }
+}
---
+++ Randominator/UI/StructuredDataValueAttribute.cs
@@ -0,0 +1,13 @@
+﻿namespace TehGM.Randominator.UI
+{
+    [AttributeUsage(AttributeTargets.Field, AllowMultiple = false)]
+    public class StructuredDataValueAttribute : Attribute
+    {
+        public string Value { get; }
+
+        public StructuredDataValueAttribute(string value) : base()
+        {
+            this.Value = value;
+        }
+    }
+}
