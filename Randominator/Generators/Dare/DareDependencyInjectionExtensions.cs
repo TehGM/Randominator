1bcd9d7bfdd62bc63442ba77d9ffa093945c3bf3
TehGM <TehGM@users.noreply.github.com>
2022-03-17 11:26:07 +0000

    Fix dare generator DI extensions use wrong options type

--- Randominator/Generators/Dare/DareDependencyInjectionExtensions.cs
+++ Randominator/Generators/Dare/DareDependencyInjectionExtensions.cs
@@ -1,12 +1,11 @@
 ﻿using TehGM.Randominator.Generators.Dare;
 using TehGM.Randominator.Generators.Dare.Services;
-using TehGM.Randominator.Generators.MobileGameName;
 
 namespace Microsoft.Extensions.DependencyInjection
 {
     public static class DareDependencyInjectionExtensions
     {
-        public static IServiceCollection AddDareGenerator(this IServiceCollection services, Action<MobileGameNameOptions> configureOptions = null)
+        public static IServiceCollection AddDareGenerator(this IServiceCollection services, Action<DareGeneratorOptions> configureOptions = null)
         {
             if (configureOptions != null)
                 services.Configure(configureOptions);
